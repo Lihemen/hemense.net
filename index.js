d1f293aed3202c914f46848f03f7b64c69c7d250
Hemense Lan <lihemen@gmail.com>
2022-05-08 19:16:22 +0100

    75%

--- index.js
+++ index.js
@@ -16,32 +16,66 @@ const changing = new Typed("#changing", {
   typeSpeed: 200,
 });
 
-const side_bar_links = document.querySelectorAll(".sidebar__menu__link");
-console.log(side_bar_links);
-const sections = document.querySelectorAll("section");
+const my_alert = `
+<div class="alert">
+  <div class="alert__error">
+    <span class="alert__text">Success</span>
+    <span><i class="fa-solid fa-close"></i></span>
+  </div>
+</div>
+`;
 
-// window.addEventListener("scroll", function (e) {
-//   sections.forEach((section) => {
-//     if (isInViewPort(section)) {
-//       console.log(section.id);
-//     } else {
-//       // section.classList.remove("active");
-//     }
-//   });
-// });
+document.querySelector("#alert__close").addEventListener("click", function () {
+  document.querySelector(".alert").remove();
+});
+
+document.querySelector("#age").innerHTML = new Date().getFullYear() - 1999;
+
+const sections = document.querySelectorAll("section");
+const links = document.querySelectorAll(".sidebar__menu__link");
+const scroller = document.querySelector("#scrollTop");
+
+links.forEach((link) => {
+  link.addEventListener("click", function () {
+    if (link.parentElement.classList.contains("active")) {
+      return;
+    }
+
+    setActive(link);
+  });
+});
+
+window.addEventListener("scroll", function (e) {
+  for (let i = 0; i < sections.length; i++) {
+    const id = sections[i].id;
+    if (isInViewPort(sections[i])) {
+      const link = document.querySelector(`a[href="#${id}"]`);
+      setActive(link);
+      if (id !== "") {
+        scroller.classList.add("show");
+      } else {
+        scroller.classList.remove("show");
+      }
+    }
+  }
+});
 
 const setActive = (el) => {
-  [...el.parentElement.children].forEach((sib) =>
-    sib.classList.remove("active")
-  );
-  el.classList.add("active");
+  [...el.parentElement.parentElement.children].forEach((sib) => {
+    sib.classList.remove("active");
+  });
+  el.parentElement.classList.add("active");
 };
 
 const isInViewPort = (el) => {
-  const elTop = el.getBoundingClientRect().top;
-  const elBottom = elTop + window.innerHeight;
-  const vpTop = window.scrollY;
-  const vpBottom = window.outerHeight;
+  var bounding = el.getBoundingClientRect();
 
-  return elBottom > vpTop && elTop < vpBottom;
+  const { top, left, bottom, right } = bounding;
+  const vpHeight = document.documentElement.clientHeight;
+  const vpWidth = document.documentElement.clientWidth;
+  return top >= 0 && left >= 0 && bottom <= vpHeight && right <= vpWidth;
 };
+
+scroller.addEventListener("click", () => {
+  window.scroll({ top: 0, left: 0, behavior: "smooth" });
+});
